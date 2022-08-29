9027df0fd26fe51428ea4333cce9f981e0f4d195
dhruv2253 <dhruv225@outlook.com>
2022-08-29 02:12:25 -0400

    added buttons and few messages

--- javascript.js
+++ javascript.js
@@ -1,4 +1,23 @@
-//#region Computer choice function
+//Buttons declaration
+const buttons = document.querySelectorAll('[data-selection]');
+const scoreOfPlayer = document.querySelector('.player-score');
+const scoreOfComp = document.querySelector('.comp-score');
+const roundMessage = document.querySelector('.message');
+
+
+
+buttons.forEach((button)=> {
+    button.addEventListener('click',e => {
+        const selectionName = button.dataset.selection
+        if (playerWin >=5 || compWin >= 5)
+        return;
+
+        game(selectionName);
+})})
+
+
+
+
 // function to return the choice of the computer
 function getComputerChoice(){
     // List of possible choices in the game 
@@ -7,118 +26,73 @@ function getComputerChoice(){
     const result = choices[Math.floor(Math.random() * choices.length)];
     return result;
 }
-//#endregion
 
 //#region Variables and function to play the round
 // Variable to count how many wins the player has
 let playerWin = 0;
 // Variable to count how many wins the computer has
 let compWin = 0;
-//Variable to count how many ties occured 
-let tie = 0;
-// Variable that initializes the winner of each round
-let roundWinner = ''
+
 // Function to play the round 
 function playRound(playerSelection, computerSelection){
 
     // Ff player chooses rock and computer chooses paper
-    if (playerSelection.toLowerCase() === "rock" && computerSelection === "Paper"){
+    if (playerSelection === "rock" && computerSelection === "Paper"){
         // Display what happens, and increment compWin by 1
-        console.log("You Lose! Paper beats rock");
+        
+        roundMessage.textContent = "You Lose! Paper beats rock";
         compWin++;
-        // round winner is...
-        roundWinner = 'Computer'
     }
     // If player chooses rock and computer chooses scissors
-    else if (playerSelection.toLowerCase() === "rock" && computerSelection === "Scissors"){
+    else if (playerSelection === "rock" && computerSelection === "Scissors"){
        // Display what happens, and increment playerWin by 1
-        console.log("You Win! Rock beats scissors");
+       roundMessage.textContent = "You Win! Rock beats scissors";
         playerWin++;
-        // round winner is...
-        roundWinner = 'Player'
+
     }
     // If player chooses paper and computer chooses scissors
-    else if (playerSelection.toLowerCase() === "paper" && computerSelection === "Scissors"){
+    else if (playerSelection === "paper" && computerSelection === "Scissors"){
         // Display what happens, and increment compWin by 1
-        console.log("You Lose! Scissors beat paper");
+        roundMessage.textContent = "You Lose! Scissors beat paper";
         compWin++;
-         // round winner is...
-        roundWinner = 'Computer'
     }
     // If player chooses paper and computer chooses rock
-    else if (playerSelection.toLowerCase() === "paper" && computerSelection === "Rock"){
+    else if (playerSelection === "paper" && computerSelection === "Rock"){
         // Display what happens, and increment playerWin by 1
-        console.log("You Win! Paper beats rock");
-        playerWin++;
-         // round winner is...
-        roundWinner = 'Player'
+        roundMessage.textContent = "You Win! Paper beats rock";
+        playerWin++;  
     }
     // If player chooses scissors and computer chooses paper
-    else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "Paper"){
+    else if (playerSelection  === "scissors" && computerSelection === "Paper"){
         // Display what happens, and increment playerWin by 1
-        console.log("You Win! Scissors beats paper");
-        playerWin++;
-         // round winner is...
-        roundWinner = 'Player'
+        roundMessage.textContent = "You Win! Scissors beats paper";
+        playerWin++; 
     }
     // If player chooses scissors and computer chooses rock
-    else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "Rock"){
+    else if (playerSelection === "scissors" && computerSelection === "Rock"){
         // Display what happens, and increment compWin by 1
-        console.log("You Lose! Rock beats scissors");
-        compWin++;
-         // round winner is...
-        roundWinner = 'Computer'
+        roundMessage.textContent = "You Lose! Rock beats scissors";
+        compWin++;  
     }
     // If player and computer choose the same thing
-    else if (playerSelection === computerSelection){
-        // Display what happens, and increment tie by 1
-        console.log("Draw!");
-        tie++;
-         // round winner is...
-        roundWinner = "tie";
-    }
-    // Display winner of round
-    console.log(`Winner of round: ${roundWinner}`);
-  
-}
-//#endregion
-
-//#region Who won the game function
-// function to determine who wins the entire game
-function whoWon(playerWin, compWin, tie){
-    // If player wins more rounds, player wins
-    if (playerWin > compWin){
-        // Display player wins
-        console.log("You won the whole game!");
-    }
-    // If computer wins more rounds, computer wins
-    else if (playerWin < compWin){
-        // Display player loses
-        console.log("You lose");
-    }
-    // if amount of wins is equal
     else{
-        // Display no winner
-        console.log("NO Winner");
+        // Display what happens, and increment tie by 1
+        roundMessage.textContent = "Draw!";  
     }
+
+    // Display the score 
+
+    scoreOfPlayer.textContent = playerWin;
+    scoreOfComp.textContent = compWin;
 }
-//#endregion
 
-//#region Main game function to play the game
-function game(){
-    for (let i = 0; i < 5; i++){
-        
-        const playerSelection = prompt("Rock Paper Scissors?");
-        const computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection);
-        
-        
-    }
-    whoWon(playerWin, compWin, tie,);
-
+// Main game function to play the game
+function game(playerSelect){
+ 
+    let playerSelection = playerSelect
+    let computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+    
+    
 }
-//#endregion
 
-// Play the game.
-
-game();
